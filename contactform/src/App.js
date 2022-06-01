dd52135bfeaf65a9572395bb9cea03a566fb4b48
Hossein <djalicrt@gmail.com>
2022-06-01 10:56:43 +0430

    css style and global stuff is done

--- contactform/src/App.js
+++ contactform/src/App.js
@@ -2,6 +2,7 @@
 import NotesList from "./components/NotesList";
 import {useState} from "react";
 import {nanoid} from 'nanoid';
+import Search from "./components/Search";
 
 
 const App = () => {
@@ -33,6 +34,8 @@ const App = () => {
     },
 ]);
 
+  const [searchText , setSearchText] = useState('');
+
   const addNote = (text) => {
     const date = new Date()
     const newNote = {
@@ -44,11 +47,19 @@ const App = () => {
     setNotes(newNotes)
   }
 
+  const deleteNote = (id) => {
+    const newNotes = notes.filter((note) => note.id !== id)
+    setNotes(newNotes)
+  }
+
 
   return (
     <div className="container">
-      <NotesList notes={notes}
-       handleAddNote={addNote} />
+      <Search handleSearchNote={setSearchText} />
+      <NotesList notes={notes.filter((note) => note.text.toLowerCase().includes(searchText))}
+       handleAddNote={addNote}
+       handleDeleteNote = {deleteNote}
+       />
     </div>
 
   )
